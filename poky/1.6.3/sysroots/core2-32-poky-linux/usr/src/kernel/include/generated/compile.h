91516a31dc3af66e6141f16c0ae6ff24126fae10
solar <solar@localhost.localdomain>
2015-08-18 14:57:11 +0800

    Intel Rengeley toolchain

---
+++ poky/1.6.3/sysroots/core2-32-poky-linux/usr/src/kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#1 SMP Tue Aug 18 10:44:52 CST 2015"
+#define LINUX_COMPILE_BY "elliot"
+#define LINUX_COMPILE_HOST "localhost.localdomain"
+#define LINUX_COMPILER "gcc version 4.8.2 (GCC) "
